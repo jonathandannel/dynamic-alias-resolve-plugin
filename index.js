1eeefc81cc611b6d8ee4e028a6a67a7453436a1f
jonathan dannel <usixjad@gmail.com>
2022-01-05 11:31:03 -0500

    add support for resolving /index.ext + only honor ext with length of 2-3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
 module.exports = class DynamicAliasResolvePlugin {
   /**
@@ -31,105 +31,112 @@ module.exports = class DynamicAliasResolvePlugin {
    * @param {Array<string>} param0.extensions specify the extensions that we should try for modules that don't have file extensions
    */
   constructor({ alias, dynamic, pattern, extensions } = {}) {
-    if (typeof alias === "string") {
-      alias = [alias];
+    if (typeof alias === 'string') {
+      alias = [alias]
     }
 
     const defaultOptions = {
-      alias: ["@"],
+      alias: ['@'],
       dynamic: () => null,
       pattern: /.*/,
-      extensions: ["js", "mjs", "ts", "jsx", "tsx"],
-    };
+      extensions: ['js', 'mjs', 'ts', 'jsx', 'tsx']
+    }
 
     this.options = extend(defaultOptions, {
       alias,
       dynamic,
       pattern,
-      extensions,
-    });
+      extensions
+    })
   }
 
   apply(resolver) {
-    const { alias, dynamic, pattern, extensions } = this.options;
+    const { alias, dynamic, pattern, extensions } = this.options
 
     if (alias.length === 0) {
-      return;
+      return
     }
 
     resolver
-      .getHook("described-resolve")
-      .tapAsync("DynamicAliasResolvePlugin", (request, resolveContext, callback) => {
-        /**
-         * @type {string}
-         */
-        const innerRequest = request.request || request.path;
-        // For files that shouldn't be handled by this plugin
-        if (!innerRequest || !pattern.test(innerRequest)) {
-          return callback();
-        }
-
-        for (const name of alias) {
-          if (innerRequest.startsWith(name)) {
-            const dynamicPath = dynamic(request, name);
-            if (!dynamicPath) {
-              continue;
-            }
-
-            let newRequestPath = path.resolve(dynamicPath, innerRequest.substr(name.length + 1));
-            if (!hasExtension(newRequestPath)) {
-              const newRequestPathsWithExtension = getNewRequestPathsWithExtension(
-                newRequestPath,
-                extensions
-              );
-              newRequestPath = newRequestPathsWithExtension.find((filePath) =>
-                fs.existsSync(filePath)
-              );
-            }
-
-            if (!fs.existsSync(newRequestPath)) {
-              continue;
-            }
-
-            const obj = Object.assign({}, request, {
-              request: newRequestPath,
-            });
-            return resolver.doResolve(
-              resolver.getHook("resolve"),
-              obj,
-              `DynamicAliasResolvePlugin ${newRequestPath}`,
-              resolveContext,
-              (err, result) => {
-                if (err) return callback(err);
-                // Don't allow other aliasing or raw request
-                if (result === undefined) return callback(null, null);
-                return callback(null, result);
-              }
-            );
+      .getHook('described-resolve')
+      .tapAsync(
+        'DynamicAliasResolvePlugin',
+        (request, resolveContext, callback) => {
+          /**
+           * @type {string}
+           */
+          const innerRequest = request.request || request.path
+          // For files that shouldn't be handled by this plugin
+          if (!innerRequest || !pattern.test(innerRequest)) {
+            return callback()
           }
+
+          for (const name of alias) {
+            if (innerRequest.startsWith(name)) {
+              const dynamicPath = dynamic(request, name)
+              if (!dynamicPath) {
+                continue
+              }
+
+              let newRequestPath = path.resolve(
+                dynamicPath,
+                innerRequest.substr(name.length + 1)
+              )
+              if (!hasExtension(newRequestPath)) {
+                const newRequestPathsWithExtension =
+                  getNewRequestPathsWithExtension(newRequestPath, extensions)
+                newRequestPath = newRequestPathsWithExtension.find(filePath =>
+                  fs.existsSync(filePath)
+                )
+              }
+
+              if (!fs.existsSync(newRequestPath)) {
+                continue
+              }
+
+              const obj = Object.assign({}, request, {
+                request: newRequestPath
+              })
+              return resolver.doResolve(
+                resolver.getHook('resolve'),
+                obj,
+                `DynamicAliasResolvePlugin ${newRequestPath}`,
+                resolveContext,
+                (err, result) => {
+                  if (err) return callback(err)
+                  // Don't allow other aliasing or raw request
+                  if (result === undefined) return callback(null, null)
+                  return callback(null, result)
+                }
+              )
+            }
+          }
+
+          return callback()
         }
-
-        return callback();
-      });
+      )
   }
-};
+}
 
-function hasExtension(filePath = "") {
-  // we don't care about the extension is valid or not, just a dot plus a word whose length is between 1 and 10.
-  const extensionPattern = /\.[a-z]{1,10}$/i;
-  return extensionPattern.test(filePath);
+function hasExtension(filePath = '') {
+  const extensionPattern = /\.[a-z]{2,3}$/i
+  return extensionPattern.test(filePath)
 }
 
 function getNewRequestPathsWithExtension(newRequestPath, extensions) {
-  return extensions.map((ext) => `${newRequestPath}.${ext}`);
+  return [
+    ...extensions.map(ext => `${newRequestPath}.${ext}`),
+    ...extensions.map(ext => `${newRequestPath + '/index'}.${ext}`)
+  ]
 }
 
 function extend(src, dest) {
   return Object.keys(dest).reduce((src, curKey) => {
-    const val = dest[curKey];
+    const val = dest[curKey]
     if (val !== undefined) {
-      src[curKey] = val;
+      src[curKey] = val
     }
-    return src;
-  }, src);
+    return src
+  }, src)
 }
+
